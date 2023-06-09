9942c7abca623efc56c35d63829ee102c0b171a4
Christophe BAJAN <108456770+ChristopheBaj@users.noreply.github.com>
2023-06-09 14:47:38 +0900

    Update issue.py

--- issue.py
+++ issue.py
@@ -8,5 +8,5 @@ else:
 	st.write("st.session_state = ",st.session_state["proof"])
 reset_button=st.button("Reset session_state")
 if reset_button:
-	st.session_state={}
+	st.session_state.clear()
 	st.experimental_rerun()

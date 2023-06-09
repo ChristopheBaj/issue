b3d9c22679b673e6605b666b08212d411ad72111
Christophe BAJAN <108456770+ChristopheBaj@users.noreply.github.com>
2023-06-09 11:28:26 +0900

    Create issue.py

---
+++ issue.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+if 'proof' not in st.session_state:
+	st.session_state["proof"]=1
+	st.write(st.session_state["proof"])
+	st.stop()
+else:
+	st.session_state["proof"]+=1
+	st.write(st.session_state["proof"])
+reset_button=st.button("Reset session_state")
+if reset_button:
+	st.session_state={}
+	st.experimental_rerun()

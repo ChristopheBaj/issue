1108303749af076decb0a793ac350cc1e9c28423
Christophe BAJAN <108456770+ChristopheBaj@users.noreply.github.com>
2023-06-09 11:32:48 +0900

    Update issue.py

--- issue.py
+++ issue.py
@@ -2,11 +2,11 @@ import streamlit as st
 
 if 'proof' not in st.session_state:
 	st.session_state["proof"]=1
-	st.write("st.session_state = "st.session_state["proof"])
+	st.write("st.session_state = ",st.session_state["proof"])
 	st.stop()
 else:
 	st.session_state["proof"]+=1
-	st.write("st.session_state = "st.session_state["proof"])
+	st.write("st.session_state = ",st.session_state["proof"])
 reset_button=st.button("Reset session_state")
 if reset_button:
 	st.session_state={}
